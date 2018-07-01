2ea05c530f0b0a07f1ff0fd73b85e635cda3e21f
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-01 19:21:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  servers = [
+    {
+      name:'TestServer',
+      capacity: 10,
+      id: this.generateId()
+    },
+    {
+      name:'Liveserver',
+      capacity: 100,
+      id: this.generateId()
+    }
+  ];
+
+  onAddServer(name: string){
+    this.servers.push({
+      name: name,
+      capacity: 50,
+      id:this.generateId()
+    });
+  }
+
+  private generateId(){
+    return Math.round(Math.random() * 10000);
+  }
+
 }
